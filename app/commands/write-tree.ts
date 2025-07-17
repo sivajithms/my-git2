7e24de67449a08e8bde9eb7a9f9a9ec72cadc59a
sivajithms <sivajithm@gmail.com>
2025-07-17 16:47:57 +0530

    bug fix

--- app/commands/write-tree.ts
+++ app/commands/write-tree.ts
@@ -3,5 +3,5 @@ import { recursiveCreateTree } from '../utils/tree-utils';
 export function handleWriteTreeCommand() {
   const basePath = process.cwd();
   const treeHash = recursiveCreateTree(basePath);
-  process.stdout.write(treeHash);
+  console.log(treeHash);
 }
